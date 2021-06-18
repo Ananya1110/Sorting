55355b683b554a32a790449f5c830c4c23ac0fe3
Ananya1110 <80620730+Ananya1110@users.noreply.github.com>
2021-06-18 10:47:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var NumbersCollection_1 = require("./NumbersCollection");
+var numbresCollection = new NumbersCollection_1.NumbersCollection([50, 3, -5, 0]);
+numbresCollection.sort();
+console.log(numbresCollection.data);
+//  const charactersCollection = new CharactersCollection('ananyX');
+//  charactersCollection.sort();
+// console.log(charactersCollection.data);
+// const numbersCollection =new NumbersCollection([50,3,-5,0]);
+// numbersCollection.sort();
+// const linkedList=new LinkedList();
+// linkedList.add(500);
+// linkedList.add(-10);
+// linkedList.add(-3);
+// linkedList.add(-4);
+// linkedList.sort();
+// linkedList.print();
